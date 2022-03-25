d40d986ee807506b5ed5d2ad10dab64ddf17d4cf
Josephine Chong <josephine.chong@myob.com>
2022-03-25 13:08:33 +1100

    State enum created

---
+++ ConwaysGameOfLife/Cell.cs
@@ -0,0 +1,7 @@
+namespace ConwaysGameOfLife
+{
+    public class Cell
+    {
+        
+    }
+}
--- ConwaysGameOfLifeTest/CellTest.cs
+++ ConwaysGameOfLifeTest/CellTest.cs
@@ -1,4 +1,5 @@
 using System;
+using ConwaysGameOfLife;
 using Xunit;
 
 namespace ConwaysGameOfLifeTest
