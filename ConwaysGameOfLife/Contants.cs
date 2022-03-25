282308a6ab44d16d29c5849de280eba158f5c63a
Josephine Chong <josephine.chong@myob.com>
2022-03-25 14:50:13 +1100

    Constants class created to store magic numbers and Death by overcrowding function written in State Laws class and second unit test passing

---
+++ ConwaysGameOfLife/Contants.cs
@@ -0,0 +1,8 @@
+namespace ConwaysGameOfLife
+{
+    public static class Contants
+    {
+        public const int NumberOfLiveNeighboursForDeathByUnderpopulationLaw = 2;
+        public const int NumberOfLiveNeighboursForDeathByOvercrowdingLaw = 3;
+    }
+}
--- ConwaysGameOfLife/StateLaws.cs
+++ ConwaysGameOfLife/StateLaws.cs
@@ -4,17 +4,21 @@ namespace ConwaysGameOfLife
     {
         public State UpdateState(State state, int liveNeighbours)
         {
-            if (DeathByUnderpopulation(state, liveNeighbours))
+            if (DeathByUnderpopulation(state, liveNeighbours) || DeathByOvercrowding(state, liveNeighbours))
             {
                state = State.Dead;
             }
-
             return state;
         }
 
         private bool DeathByUnderpopulation(State state, int liveNeighbours)
         {
-            return state == State.Alive && liveNeighbours < 2;
+            return state == State.Alive && liveNeighbours < Contants.NumberOfLiveNeighboursForDeathByUnderpopulationLaw;
+        }
+
+        private bool DeathByOvercrowding(State state, int liveNeighbours)
+        {
+            return state == State.Alive && liveNeighbours > Contants.NumberOfLiveNeighboursForDeathByOvercrowdingLaw;
         }
     }
 }
