3b4f98c373dda220f5c718a433983f585a79d40b
Josephine Chong <josephine.chong@myob.com>
2022-03-25 13:15:36 +1100

    Created Cell Class and Is Alive function, unit test passing, wrote another happy path unit test for checking dead cell, second test passing

--- ConwaysGameOfLife/Cell.cs
+++ ConwaysGameOfLife/Cell.cs
@@ -2,6 +2,11 @@ namespace ConwaysGameOfLife
 {
     public class Cell
     {
-        
+        public State State;
+
+        public bool IsAlive(State state)
+        {
+            return state == State.Alive;
+        }
     }
 }
--- ConwaysGameOfLifeTest/CellTest.cs
+++ ConwaysGameOfLifeTest/CellTest.cs
@@ -12,27 +12,29 @@ namespace ConwaysGameOfLifeTest
         {
             //arrange
             var cell = new Cell();
+            var cellState = State.Alive;
 
             //act
-            var stateIsAlive = cell.IsAlive();
+            var stateIsAlive = cell.IsAlive(cellState);
 
             //assert
             Assert.True(stateIsAlive);
         }
         
         //happy path
-        /*[Fact] 
+        [Fact] 
         public void State_For_A_Dead_Cell_Should_Be_Dead()
         {
             //arrange
             var cell = new Cell();
+            var cellState = State.Dead;
 
             //act
-            var stateIsAlive = cell.IsAlive();
+            var stateIsAlive = cell.IsAlive(cellState);
 
             //assert
             Assert.False(stateIsAlive);
-        }*/
+        }
         
         //unhappy path
     }
