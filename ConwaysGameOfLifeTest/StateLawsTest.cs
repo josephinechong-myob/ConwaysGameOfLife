c3365ae5ee4be97ff1ccff00ea6b55615c99f5ba
Josephine Chong <josephine.chong@myob.com>
2022-03-25 14:41:21 +1100

    State Law unit test for Death by overcrowding written

--- ConwaysGameOfLifeTest/StateLawsTest.cs
+++ ConwaysGameOfLifeTest/StateLawsTest.cs
@@ -21,7 +21,7 @@ namespace ConwaysGameOfLifeTest
             Assert.Equal(expectedCellState, updatedCellState);
         }
         
-        /*[Fact]
+        [Fact]
         public void Cell_State_Should_Change_To_Dead_If_Alive_Cell_Has_Greater_Than_Three_Live_Neighbours()
         {
             //arrange
@@ -31,13 +31,13 @@ namespace ConwaysGameOfLifeTest
             var expectedCellState = State.Dead;
 
             //act
-            var updatedCellState = stateLaw.UpdateCellState(numberOfLiveNeighbours);
+            var updatedCellState = stateLaw.UpdateState(originalCellState, numberOfLiveNeighbours);
             
             //assert
             Assert.Equal(expectedCellState, updatedCellState);
         }
         
-        [Fact] //change to theory
+        /*[Fact] //change to theory
         public void Cell_State_Should_Stay_Alive_If_Alive_Cell_Has_Two_Or_Three_Live_Neighbours()
         {
             //arrange
